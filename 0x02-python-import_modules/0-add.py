11295bfc3af0cd4b44142f5f7678ff852409138b
Possy <tenyfash@yahoo.com>
2024-01-10 21:20:59 +0000

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
     a = 1
     b = 2
     from add_0 import add
-    result = add(a,b)
+    result = add(a, b)
 
 print("{} + {} = {}".format(a, b, result))
