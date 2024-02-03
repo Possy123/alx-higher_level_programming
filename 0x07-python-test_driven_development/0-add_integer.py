05b33d9fabb3b851a9ae96163b4f575eef208c12
Possy <tenyfash@yahoo.com>
2024-02-03 16:06:11 +0000

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/ python3
+"""
+    Module to sum
+    add_integer: adds two intergers or float
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add two numbers
+    Example:
+    >>> add(2, 3)
+    5
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+        a = int(a)
+        b = int(b)
+
+    return (a + b)
